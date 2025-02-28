a00bd407196cd36eb0ef7f987a5dce7f7b83db2a
Manish-Tamang <maneshtamang833@gmail.com>
2025-02-28 18:19:58 +0545

    Added Links 🥳

--- components/hero-section.tsx
+++ components/hero-section.tsx
@@ -1,5 +1,6 @@
 import Image from "next/image";
 import { FaInstagram, FaFacebook, FaTwitter, FaYoutube, FaSpotify, FaApple } from "react-icons/fa";
+import Link from 'next/link';
 
 export default function HeroSection() {
   return (
@@ -9,12 +10,24 @@ export default function HeroSection() {
         <div className="max-w-[860px] mx-auto h-full px-4 sm:px-6 lg:px-8 flex flex-col justify-end pb-4">
           <h1 className="text-3xl font-bold tracking-wider">YABESH THAPA</h1>
           <div className="flex space-x-3 mt-2">
-            <FaInstagram size={20} className="text-gray-300" />
-            <FaFacebook size={20} className="text-gray-300" />
-            <FaTwitter size={20} className="text-gray-300" />
-            <FaYoutube size={20} className="text-gray-300" />
-            <FaSpotify size={20} className="text-gray-300" />
-            <FaApple size={20} className="text-gray-300" />
+            <Link href="https://www.instagram.com/yabush69/" target="_blank" rel="noopener noreferrer">
+              <FaInstagram size={20} className="text-gray-300" />
+            </Link>
+            <Link href="https://www.facebook.com/yabeshthapamusic" target="_blank" rel="noopener noreferrer">
+              <FaFacebook size={20} className="text-gray-300" />
+            </Link>
+            <Link href="https://x.com/yabush69" target="_blank" rel="noopener noreferrer">
+              <FaTwitter size={20} className="text-gray-300" />
+            </Link>
+            <Link href="https://www.youtube.com/@YabeshThapa" target="_blank" rel="noopener noreferrer">
+              <FaYoutube size={20} className="text-gray-300" />
+            </Link>
+            <Link href="https://open.spotify.com/artist/1D0B4qrMRfjzDfBlijs3YH?si=TybZBF9yQd-KzLYGbwFBgQ" target="_blank" rel="noopener noreferrer">
+              <FaSpotify size={20} className="text-gray-300" />
+            </Link>
+            <Link href="https://music.apple.com/np/artist/yabesh-thapa/1510737030" target="_blank" rel="noopener noreferrer">
+              <FaApple size={20} className="text-gray-300" />
+            </Link>
           </div>
         </div>
       </div>
